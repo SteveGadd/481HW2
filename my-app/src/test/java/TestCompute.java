da53336d55fd7a901ab9cce14a015a033feae1b2
SteveGadd <janagircan@hotmail.com>
2024-03-31 07:00:39 +0300

    Achieved code coverage %100

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -1,5 +1,3 @@
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -7,11 +5,48 @@ import org.junit.Test;
 
 public class TestCompute {
   Compute c;
-
+  MessageQueue mq;
+    
   @Test
-  public void example() {
-    MessageQueue mq = mock(MessageQueue.class);
-    c = new Compute(mq);
-    assertTrue(true);
-  }
+    public void testIsEmpty() {
+        mq = mock(MessageQueue.class);
+        c = new Compute(mq);
+        when(mq.size()).thenReturn(0);
+        assertEquals(-1, c.countNumberOfOccurrences("test"));
+    }
+
+    @Test
+    public void testElementNotPresent() {
+        mq = mock(MessageQueue.class);
+        c = new Compute(mq);
+        when(mq.size()).thenReturn(3);
+        when(mq.contains("test")).thenReturn(false);
+        assertEquals(0, c.countNumberOfOccurrences("test"));
+    }
+
+    @Test
+    public void testElementPresentOnce() {
+        mq = mock(MessageQueue.class);
+        c = new Compute(mq);
+        when(mq.size()).thenReturn(3);
+        when(mq.contains("test")).thenReturn(true);
+        when(mq.getAt(0)).thenReturn("test");
+        when(mq.getAt(1)).thenReturn("other");
+        when(mq.getAt(2)).thenReturn("another");
+        assertEquals(1, c.countNumberOfOccurrences("test"));
+    }
+
+    @Test
+    public void testElementPresentMultipleTimes() {
+        mq = mock(MessageQueue.class);
+        c = new Compute(mq);
+        when(mq.size()).thenReturn(5);
+        when(mq.contains("test")).thenReturn(true);
+        when(mq.getAt(0)).thenReturn("test");
+        when(mq.getAt(1)).thenReturn("other");
+        when(mq.getAt(2)).thenReturn("test");
+        when(mq.getAt(3)).thenReturn("another");
+        when(mq.getAt(4)).thenReturn("test");
+        assertEquals(3, c.countNumberOfOccurrences("test"));
+    }
 }
