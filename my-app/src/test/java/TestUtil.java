954208b3a7a10f26fd4681be675326bf0bfac53f
SteveGadd <janagircan@hotmail.com>
2024-03-31 06:43:45 +0300

    TestUtil covered %100

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -15,19 +15,24 @@ public class TestUtil {
     assertFalse(c.compute(1));
 }
 
-@Test
-public void testCompute_LengthEven() {
-    assertFalse(c.compute(1, 2, 3, 4));
-}
+  @Test
+  public void testCompute_LengthEven() {
+      assertFalse(c.compute(1, 2, 3, 4));
+  }
 
-@Test(expected = RuntimeException.class)
-public void testCompute_ZeroElement() {
-    c.compute(1, 2, 0, 4, 5);
-}
+  @Test(expected = RuntimeException.class)
+  public void testCompute_ZeroElement() {
+      c.compute(1, 2, 0, 4, 5);
+  }
 
 
-@Test
-public void testCompute_SumDivisible() {
-    assertTrue(c.compute(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55));
-}
+  @Test
+  public void testCompute_SumDivisible() {
+      assertTrue(c.compute(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55));
+  }
+  @Test
+  public void testCompute_elementsOdd() {
+      assertFalse(c.compute(2, 8, 11));
+  }
+
 }
