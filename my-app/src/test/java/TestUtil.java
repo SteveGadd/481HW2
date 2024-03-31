26af3fba6286be5ddd30113b4ac435e65dc56d30
SteveGadd <janagircan@hotmail.com>
2024-03-31 06:21:18 +0300

    Initial commit

---
+++ my-app/src/test/java/TestUtil.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUtil {
+  Util c;
+
+  @Before
+  public void setUp() { c = new Util(); }
+
+  @Test
+  public void testCompute_LengthOne() {
+    assertFalse(c.compute(1));
+}
+
+@Test
+public void testCompute_LengthEven() {
+    assertFalse(c.compute(1, 2, 3, 4));
+}
+
+@Test(expected = RuntimeException.class)
+public void testCompute_ZeroElement() {
+    c.compute(1, 2, 0, 4, 5);
+}
+
+
+@Test
+public void testCompute_SumDivisible() {
+    assertTrue(c.compute(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55));
+}
+}
